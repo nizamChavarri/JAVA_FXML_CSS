dcc4ec5f350d9f2657112488411b7a5515fe03df
nizamChavarri <nizamchavarri@gmail.com>
2025-09-13 16:05:58 +0200

    primer commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -19,7 +19,8 @@ public class App extends Application {
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         launch();
     }
 }
